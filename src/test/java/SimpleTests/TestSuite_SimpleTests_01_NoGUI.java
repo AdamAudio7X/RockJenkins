2f0f1559eac1e1649be693fabe609ecccda078b8
AdamAudio7X <peterklimqa@gmail.com>
2023-12-12 19:59:25 -0800

    1st commit - 1st set of files to local git

---
+++ src/test/java/SimpleTests/TestSuite_SimpleTests_01_NoGUI.java
@@ -0,0 +1,19 @@
+package SimpleTests;
+
+import org.testng.annotations.Test;
+
+public class TestSuite_SimpleTests_01_NoGUI {
+	
+	@Test()
+	public void Test_Print_01()
+	{
+		System.out.println("This came from Test_Print_01");
+	}
+	
+	@Test()
+	public void Test_Print_02()
+	{
+		System.out.println("This came from Test_Print_02");
+	}
+
+}
